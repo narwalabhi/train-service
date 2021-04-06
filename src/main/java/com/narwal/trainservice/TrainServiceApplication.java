8a9eb406414cb62217d732f2853109d40c1b70e0
narwalabhi <anarwal500@gmail.com>
2021-04-06 23:49:05 +0530

    Eureka Service Discovery Added

--- src/main/java/com/narwal/trainservice/TrainServiceApplication.java
+++ src/main/java/com/narwal/trainservice/TrainServiceApplication.java
@@ -2,9 +2,19 @@ package com.narwal.trainservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TrainServiceApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(TrainServiceApplication.class, args);
