a8cb89c107358e7d30781b0e253880106146148c
narwalabhi <anarwal500@gmail.com>
2021-04-25 13:38:14 +0530

    TripSchedule changes.

--- src/main/java/com/narwal/trainservice/model/TripScheduleSearchReqBody.java
+++
@@ -1,9 +0,0 @@
-package com.narwal.trainservice.model;
-
-import java.time.LocalDate;
-
-public class TripScheduleSearchReqBody {
-    private String froCode;
-    private String toCode;
-    private LocalDate date;
-}
